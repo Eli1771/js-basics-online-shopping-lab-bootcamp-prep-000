4e2a12b47634b9144cf7500668a25d5344919fae
elijahmm17@gmail.com <elijahmm17@gmail.com>
2018-01-15 20:08:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (!cardNumber.length) {
+  if (!cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.");
   } else {
     var cost = total();
